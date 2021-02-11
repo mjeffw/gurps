e51de67f1f8884bf3e7073cbf100f8eb5bb9a6cd
Trevor Richards <trev@trevdev.ca>
2021-02-11 07:33:39 -0800

    rename types folder to typings

---
+++ src/ts/typings/foundry.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Runtime configuration settings for Foundry VTT which exposes a large number of variables which determine how aspects of the software behaves.
+ * https://foundryvtt.com/api/global.html#CONFIG
+ */
+declare const CONFIG: { [key: string]: any }
