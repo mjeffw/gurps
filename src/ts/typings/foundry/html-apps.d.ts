290e32111189f4affba3ab43d3e321e84da5e46d
Trevor Richards <trev@trevdev.ca>
2021-02-14 22:07:01 -0800

    add application

--- src/ts/typings/foundry/html-apps.d.ts
+++ src/ts/typings/foundry/html-apps.d.ts
@@ -1,9 +1,50 @@
 /**
+ * The standard application window that is rendered for a large variety of UI 
+ * elements in Foundry VTT.  
+ * https://foundryvtt.com/api/Application.html
+ */
+class Application {
+  constructor(options={}){
+    this.options = mergeObject(
+      Application.defaultOptions,
+      options,
+      {
+        insertKeys: true,
+        insertValues: true,
+        overwrite: true,
+        inplace: false
+      }
+    )
+  }
+  
+  static get defaultOptions() {
+    return {
+      baseApplication: null,
+      width: null,
+      height: null,
+      top: null,
+      left: null,
+      popOut: true,
+      minimizable: true,
+      resizable: false,
+      id: "",
+      classes: [],
+      dragDrop: [],
+      tabs: [],
+      filters: [],
+      title: "",
+      template: null,
+      scrollY: []
+    }
+  }
+}
+
+/**
  * An abstract pattern for defining an Application responsible for updating some
  * object using an HTML form
  * https://foundryvtt.com/api/FormApplication.html
  */
-class FormApplication {}
+class FormApplication extends Application {}
 
 /**
  * Extend the FormApplication pattern to incorporate specific logic for viewing
